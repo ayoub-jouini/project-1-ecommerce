8f21daece9bc339b952285748f6a5bc34ed7b44f
ayoub_jouini <ayoubjouini135@gmail.com>
2021-09-18 00:42:23 +0100

    categories controllers

---
+++ controllers/categories-controllers.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const { validationResult } = require('express-validator');
+
+const HttpError = require('../models/http-error');
+const Category = require('../models/category-model');
+const Product = require('../models/products-model');
+
+const getAllCategories = () => {
+
+}
+
+const postCategory = () => {
+
+}
+
+const updateCategory = () => {
+
+}
+
+const deleteCategory = () => {
+
+}
+
+exports.getAllCategories = getAllCategories;
+exports.postCategory = postCategory;
+exports.updateCategory = updateCategory;
+exports.deleteCategory = deleteCategory;
