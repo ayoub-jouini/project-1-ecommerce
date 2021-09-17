17f7fc9fbabc604bd8ccb0522b175003e9bc2af2
ayoub_jouini <ayoubjouini135@gmail.com>
2021-09-17 23:34:31 +0100

    user model

---
+++ models/users-model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    firsName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true, minlength: 8 },
+    image: { type: String, required: true },
+    userType: { type: String, required: true }
+});
+
+module.exports = mongoose.model('User', userSchema);
