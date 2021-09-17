434e253ee2ebe65c4f9235d8650e1f1090d0bec1
ayoub_jouini <ayoubjouini135@gmail.com>
2021-09-18 00:42:56 +0100

    users controllers

---
+++ controllers/users-controllers.js
@@ -0,0 +1,32 @@
+const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const HttpError = require('../models/http-error');
+const User = require('../models/users-model');
+
+const getAllUsers = () => {
+
+}
+
+const getUserById = () => {
+
+}
+
+const postUser = () => {
+
+}
+
+const updateUser = () => {
+
+}
+
+const deleteUser = () => {
+
+}
+
+exports.getAllUsers = getAllUsers;
+exports.getUserById = getUserById;
+exports.postUser = postUser;
+exports.updateUser = updateUser;
+exports.deleteUser = deleteUser;
