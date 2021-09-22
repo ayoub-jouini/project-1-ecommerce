f62726a8e31d4d1f5079c512aab5f78a0a4382dc
ayoub_jouini <ayoubjouini135@gmail.com>
2021-09-22 12:05:02 +0100

    signin function

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -123,8 +123,58 @@ const signUp = async (req, res, next) => {
     //the token...
 }
 
-const signIn = () => {
+const signIn = async (req, res, next) => {
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+        return next(
+            new HttpError(
+                'invalid inputs passed, please check your data',
+                422
+            )
+        );
+    }
 
+    const { email, password } = req.body;
+
+    let existingUser;
+    try {
+        existingUser = await User.findOne({ email: email });
+    } catch (err) {
+        const error = new HttpError(
+            'Logging in failed, please try again later.',
+            500
+        );
+        return next(error);
+    }
+
+    if (!existingUser) {
+        const error = new HttpError(
+            'Invalid credentials, could not log you in.',
+            403
+        );
+        return next(error);
+    }
+
+    let isValidPassword;
+    try {
+        isValidPassword = await bcrypt.compare(password, existingUser.password);
+    } catch (err) {
+        const error = new HttpError(
+            'Could not log you in, please check your credentials and try again.',
+            500
+        );
+        return next(error);
+    }
+
+    if (!isValidPassword) {
+        const error = new HttpError(
+            'Invalid credentials, could not log you in.',
+            403
+        );
+        return next(error);
+    }
+
+    //token...
 }
 
 const updateUser = () => {
