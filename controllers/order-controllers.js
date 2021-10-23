d649d9fb5b8135fe2d39ec827a8b8ee0484bd237
ayoub_jouini <ayoubjouini135@gmail.com>
2021-10-23 22:49:14 +0100

    order controllers

---
+++ controllers/order-controllers.js
@@ -0,0 +1,29 @@
+const HttpError = require('../middleware/http-error');
+const Order = require('../models/order-model');
+const User = require('../models/users-model');
+
+const postOrder = (req, res, next) => {
+
+}
+
+const getAllOrders = (req, res, next) => {
+
+}
+
+const getOrderById = (req, res, next) => {
+
+}
+
+const deleteOrder = (req, res, next) => {
+
+}
+
+const updateOrderState = (req, res, next) => {
+
+}
+
+exports.postOrder = postOrder;
+exports.getAllOrders = getAllOrders;
+exports.getOrderById = getOrderById;
+exports.deleteOrder = deleteOrder;
+exports.updateOrderState = updateOrderState;
