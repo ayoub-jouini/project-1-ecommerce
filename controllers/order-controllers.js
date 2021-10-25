32f2d277aa5339907cdf979f84af96fa6cf8ec44
ayoub_jouini <ayoubjouini135@gmail.com>
2021-10-25 15:16:02 +0100

    postOrders function

--- controllers/order-controllers.js
+++ controllers/order-controllers.js
@@ -1,9 +1,68 @@
 const HttpError = require('../middleware/http-error');
 const Order = require('../models/order-model');
+const Product = require('../models/products-model');
 const User = require('../models/users-model');
 
-const postOrder = (req, res, next) => {
+const postOrder = async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return next(
+            new HttpError('Invalid inputs passed, please check your data.', 422)
+        );
+    }
 
+    //the req body
+    const {
+        userName,
+        userEmail,
+        userPhoneNumber,
+        userAdress,
+        productsIds,
+        price
+    } = req.body;
+
+    //products validation 
+    for (let i = 0; i < productsIds.length; i++) {
+        let ValidProduct;
+        try {
+            ValidProduct = await Product.findById(productsIds[i]);
+        } catch (err) {
+            const error = new HttpError(
+                'Something went wrong, could not find product.',
+                500
+            );
+            return next(error);
+        }
+
+        if (ValidProduct.length === 0) {
+            const error = new HttpError(
+                'product not found.',
+                404
+            );
+            return next(error);
+        }
+    }
+
+    const createdOrder = new Order({
+        userName,
+        userEmail,
+        userPhoneNumber,
+        userAdress,
+        productsIds,
+        price
+    })
+
+    try {
+        await createdOrder.save();
+    } catch (err) {
+        const error = new HttpError(
+            'Something went wrong, could not save the data.',
+            500
+        );
+        return next(error);
+    }
+
+    res.status(201).json({ order: createdOrder });
 }
 
 const getAllOrders = (req, res, next) => {
