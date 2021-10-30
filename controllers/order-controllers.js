d0e5e6e42dce2de714b6ae1b77759c822c758088
ayoub_jouini <ayoubjouini135@gmail.com>
2021-10-30 17:50:02 +0100

    getAllOrders function

--- controllers/order-controllers.js
+++ controllers/order-controllers.js
@@ -65,7 +65,31 @@ const postOrder = async (req, res, next) => {
     res.status(201).json({ order: createdOrder });
 }
 
-const getAllOrders = (req, res, next) => {
+const getAllOrders = async (req, res, next) => {
+    let orders;
+    try {
+        orders = await Order.find({});
+    } catch (err) {
+        const error = new HttpError(
+            'Something went wrong, could not find the orders.',
+            500
+        );
+        return next(error);
+    }
+
+    if (orders.length == 0) {
+        const error = new HttpError(
+            'there is no orders',
+            404
+        );
+        return next(error);
+    }
+
+    res.json({
+        orders: orders.map(prod =>
+            prod.toObject({ getters: true }))
+    });
+
 
 }
 
