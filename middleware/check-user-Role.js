2fd2908d6385772e71b5b355f53bb5d080dd3319
ayoub_jouini <ayoubjouini135@gmail.com>
2021-11-11 14:15:49 +0100

    check-user-role function

--- middleware/check-user-Role.js
+++ middleware/check-user-Role.js
@@ -1,5 +1,32 @@
-const checkUserRole = (req, res, nect) => {
+const HttpError = require('../middleware/http-error');
+const User = require('../models/users-model');
 
+const checkUserRole = async (userType, userId) => {
+    let user;
+    try {
+        user = await User.findById(userId);
+    } catch (err) {
+        const error = new HttpError(
+            'Something went wrong, could not find user.',
+            500
+        );
+        return next(error);
+    }
+
+    if (!user) {
+        const error = new HttpError(
+            'could not find user.',
+            500
+        );
+        return next(error);
+    }
+
+    let valid = true;
+    if (user.userType != userType) {
+        valid = false;
+    }
+
+    return valid;
 }
 
 module.exports = checkUserRole;
